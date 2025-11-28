34835b5c42e655c849b42b58939775fc57b8db6d
rumashnirudaka1018-dev <rumashnirudaka1018@gmail.com>
2025-11-29 01:19:54 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -82,10 +82,10 @@ router.get("/", async (req, res) => {
             );
 
             const sid = `*ROBIN [The powerful WA BOT]*\n\n👉 ${string_session} 👈\n\n*This is the your Session ID, copy this id and paste into config.js file*\n\n*You can ask any question using this link*\n\n*wa.me/message/WKGLBR2PCETWD1*\n\n*You can join my whatsapp group*\n\n*https://chat.whatsapp.com/GAOhr0qNK7KEvJwbenGivZ*`;
-            const mg = `🛑 *Do not share this code to anyone* 🛑`;
+            const mg = `🛑🔥 *𝗱𝗼 𝗻𝗼𝘁 𝘀𝗵𝗮𝗿𝗲 𝘁𝗵𝗶𝘀 𝗰𝗼𝗱𝗲 𝗮𝗻𝘆𝗼𝗻𝗲* 🔥`;
             const dt = await RobinPairWeb.sendMessage(user_jid, {
               image: {
-                url: "https://raw.githubusercontent.com/Dark-Robin/Bot-Helper/refs/heads/main/autoimage/Bot%20robin%20WP.jpg",
+                url: "https://i.ibb.co/84nKdBtj/tourl-1764359174994.jpg",
               },
               caption: sid,
             });
@@ -129,3 +129,4 @@ process.on("uncaughtException", function (err) {
 });
 
 module.exports = router;
+
